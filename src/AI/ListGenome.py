9351002a6019144158addfb605299078a71494d8
AIeq <pietari.kokko@gmail.com>
2016-11-26 20:44:22 +0200

    added new ui and some genomics

--- src/AI/ListGenome.py
+++ src/AI/ListGenome.py
@@ -1,21 +1,53 @@
 from Genome import Genome
+import random
 
+colors = ["black", "white", "red", "green", "blue"]
+actions = ["left", "right", "attack" ]
+possibleGenes = colors + actions
 
 class ListGenome(Genome):  
-  def __init__(self, parents = None): #[Genome, Genome]
-        self.genes = ["ttt"]
+  def __init__(self, parents = None): #[Genome, Genome] 
+        if parents is None: 
+           self.genes = [ 
+             random.choice(possibleGenes) ,
+             random.choice(possibleGenes) ,
+             random.choice(possibleGenes) ,
+             random.choice(possibleGenes) ,
+             random.choice(possibleGenes) ,
+             random.choice(possibleGenes) 
+           ]
+        else:
+          self.genes = [
+            random.choice([parents[0].genes[0],parents[1].genes[0]]),
+            random.choice([parents[0].genes[1],parents[1].genes[1]]),
+            random.choice([parents[0].genes[2],parents[1].genes[2]]),
+            random.choice([parents[0].genes[3],parents[1].genes[3]]),
+            random.choice([parents[0].genes[4],parents[1].genes[4]]),
+            random.choice([parents[0].genes[5],parents[1].genes[5]])
+            ]
         
   def getMove(self, input): # color
+    active = False
+    for gene in self.genes:
+      if gene == input:
+        active = True
+      if active and gene in actions:
+        return gene
     return "forward";
  
   def findColor(self): # color
-    return "blue";
+    for gene in self.genes:
+      if gene in colors:
+        return gene;
+    return "black";
     
   def getPhysicalProperties(self):
     return {"color": self.findColor(), "speed":1, "health": 1, "damage": 1, "lifetime": 20};
      
   def mutate(self, propability = 10, hint = None):
-    "not implemented"
+    for i, gene in enumerate(self.genes):
+      if random.randint(0, 99) < propability:
+        self.genes[i] = random.choice(possibleGenes)  
     
   def __repr__(self):
     return str(self.genes)
---
+++ src/AI/UI2D.py
@@ -0,0 +1,149 @@
+'''A simple ui
+'''
+
+from graphics import *
+import random
+import time
+import math
+level = [["gray", "gray", "gray", "gray", "gray", "gray"],
+ ["gray", "white", "white", "white", "white", "gray"],
+ ["gray", "white", "white", "white", "white", "gray"],
+ ["gray", "white", "white", "gray", "white", "gray"],
+ ["gray", "white", "gray", "white", "white", "gray"],
+ ["gray", "white", "white", "gray", "white", "gray"],
+ ["gray", "gray", "white", "white", "gray", "gray"],
+ ["gray", "white", "gray", "white", "white", "gray"],
+ ["gray", "white", "white", "white", "gray", "gray"],
+ ["gray", "white", "white", "gray", "white", "gray"],
+ ["gray", "white", "white", "gray", "gray", "gray"],
+ ["gray", "white", "gray", "white", "white", "gray"],
+ ["gray", "gray", "white", "white", "white", "gray"],
+ ["gray", "gray", "gray", "gray", "gray", "gray"]
+]
+
+class Minion():  
+  def __init__(self, i, x,y, win): #
+        self.i = i;
+        self.x = x;
+        self.y = y;
+        self.dx = 0;
+        self.dy = 0;
+        self.color = "yellow";
+         
+        p = Point(self.x, self.y)
+        self.body = Circle(p, 20) # set center and radius
+        self.body.setFill(self.color) 
+        self.body.draw(win)
+        
+        self.label = Text(p, str(i))
+        self.label.draw(win)
+        
+        p2 = Point(self.x, self.y)
+        self.head = Circle(p2, 5) # set center and radius
+        self.head.setFill(self.color) 
+        self.head.draw(win)
+        
+        self.angle = random.randint( 0, 10);
+        self.size = 15
+        hx = self.size * math.cos(self.angle)
+        hy = self.size * math.sin(self.angle)
+        self.head.move(hx, hy)
+
+  def moveXY(self,x,y):  
+        self.x +=x
+        self.y +=y
+        self.body.move(x, y)
+        self.head.move( x, y)
+        self.label.move( x, y)
+  def move(self):  
+        x = 2*self.size * math.cos(self.angle)
+        y = 2*self.size * math.sin(self.angle)
+        self.moveXY( x, y)
+        
+  def turn(self, a):   
+        hx = -self.size * math.cos(self.angle)
+        hy = -self.size * math.sin(self.angle)
+        
+        self.angle += a * math.pi * 2 /8;
+        
+        hx += self.size * math.cos(self.angle)
+        hy += self.size * math.sin(self.angle)
+         
+        self.head.move( hx, hy)
+        
+  def collision(self, other):
+        c =  math.hypot(other.x - self.x, other.y - self.y) < self.size + other.size
+        if c:
+          self.dx = (self.x - other.x)/2
+          self.dy = (self.y - other.y)/2
+          return True
+        if self.x < 80:
+          self.dx = 80- self.x
+          c =   True
+        if self.y < 60:
+          self.dy = 60- self.y
+          c =   True
+        if self.x > 280:
+          self.dx = 280 - self.x
+          c =   True
+        if self.y > 580:
+          self.dy = 580 - self.y
+          c =   True
+        return c
+  def allign(self):
+        self.moveXY(self.dx,self.dy)
+        self.dx = 0;
+        self.dy = 0;
+           
+     
+def main():
+    win = GraphWin('game', 400, 600) # give title and dimensions
+    #win.yUp() # make right side up coordinates!
+
+    #head = Circle(Point(40,100), 25) # set center and radius
+    #head.setFill("yellow")
+    #head.draw(win)
+    
+    message = Text(Point(win.getWidth()/2, 20), 'Click anywhere to move.')
+    message.draw(win) 
+
+    for y,line in enumerate(level):
+        for x,c in enumerate(line):
+          xx = 60+40 *x
+          yy = 40+40 *y
+          box = Rectangle(Point(xx, yy), Point(xx+40, yy+40)) # set corners of  
+          box.setFill(c)
+          box.draw(win)
+    minions = [
+      Minion( 1, random.randint(56, 125), random.randint(56, 265),win),
+      Minion( 2, random.randint(26, 125), random.randint(56, 265),win),
+      Minion( 3, random.randint(26, 125), random.randint(56, 265),win),
+      Minion( 4, random.randint(26, 125), random.randint(56, 265),win),
+      Minion( 5, random.randint(26, 125), random.randint(56, 265),win),
+    ]
+    while(True):
+      for m in minions:
+        # if ai move is left then move = 1;
+        move =  random.randint(-1, 1)
+        m.turn(move);
+        m.move()
+      collision = True
+      while collision:
+        collision = False
+        for m in minions: 
+          for t in minions:
+            if m is not t:
+              c = m.collision(t)
+              #print(c)
+              if c:
+                collision = True
+        for m in minions: 
+          m.allign()
+      time.sleep(.2)
+      print("----")
+      #head = Circle(win.getMouse(), 5) # set center and radius
+      #head.setFill("yellow")
+      #head.draw(win)
+    win.close()
+
+main()
