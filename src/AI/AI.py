8b727b9757d12ddd229258b5dc194315e91cfc67
AIeq <pietari.kokko@gmail.com>
2016-11-26 14:52:58 +0200

    updated AI

---
+++ src/AI/AI.py
@@ -0,0 +1,83 @@
+ 
+from Genome import DummyGenome
+
+genomeType = "dummy"
+def genomeFactory(parents):
+  if genomeType is "dummy":
+    return DummyGenome(parents);
+  elif genomeType is "map":
+    return MapGenome(parents);
+
+# Defining a class
+class Creature:
+  def __init__(self, genome):
+        self.genome = genome
+        
+  def __repr__(self):
+    return "Creature[" + str(self.genome)+"]" 
+    
+  def move(self, input):     
+    return self.genome.getMove(input)
+    
+  def properties(self):
+    return str(self.genome.getPhysicalProperties())
+      
+class Ai:
+  def __init__(self): 
+    self.creatures = []
+    self.lastOutput = ""
+    
+  def output(self, s):
+    self.lastOutput = s
+    print(s)
+    
+  def getLastOutput(self):
+    return self.lastOutput
+    
+  def run(self):
+    running = True;  
+    self.output("AI started, give commands")
+    parents = None;
+    while(running):
+      try:
+        line = input().split(' ', 1)
+        command = line[0]
+        try:
+          args = line[1].split(' ')
+        except Exception  as e:
+          args = "none"
+
+        if command == "exit":
+          running = False;
+        elif command == "make": 
+          self.creatures = [] 
+          result = ""
+          for count in range(int(args[0])):
+            g = genomeFactory(parents)
+            
+            c = Creature(g) 
+            self.creatures.append(c)  
+            result = result + " " + c.properties()
+          
+          self.output("properties" + result) 
+        elif   command == "see":
+          result = ""
+          for a, c in zip(args, self.creatures): 
+            result = result + " " + c.move(a)
+          
+          self.output("move" + result)
+        elif command == "breed":
+          parents = [self.creatures[int(args[0])], self.creatures[int(args[1])]];
+          
+        elif command == "print":
+          self.output(str(len(self.creatures))  + " creatures " + str(self.creatures))
+        else :
+          self.output("unknown command " + command)
+      except Exception  as e:
+        self.output (" Exception:  " + str( e))
+
+if __name__ == "__main__":
+  Ai().run()
+  print("bye")
+
+  
