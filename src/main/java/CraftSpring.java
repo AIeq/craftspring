ff2662c2436a394deffd5180a529d8f4ee2adb2e
Vladimir Rogojin <vrogojin@vrogojin.net>
2016-11-26 19:34:35 +0200

    implemented async communication with python process

--- src/main/java/CraftSpring.java
+++ src/main/java/CraftSpring.java
@@ -1,5 +1,8 @@
+import net.minecraft.client.renderer.entity.RenderSilverfish;
+import net.minecraft.command.ICommand;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -18,17 +21,19 @@ public class CraftSpring {
     @Mod.Instance(MODID)
     public	static	CraftSpring	instance;
     
+    public	static	ICommand	commandHandler;
+    
     @Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-
+//    	MinecraftForge.EVENT_BUS.register(new	CraftSpringEventHandler());
 	}
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-//    	commandHandler=new	SixthSenseCommand();
-    	MinecraftForge.EVENT_BUS.register(new	CraftSpringEventHandler());
-//        ClientCommandHandler.instance.registerCommand(commandHandler);
+    	commandHandler=new	CraftSpringCommand();
+    	
+        ClientCommandHandler.instance.registerCommand(commandHandler);
     }
     
     @Mod.EventHandler
@@ -40,6 +45,7 @@ public class CraftSpring {
     {
     	EntityRegistry.registerModEntity(EntityMinion.class, "Minion", 0,
     			CraftSpring.instance, 48, 55, true, 0xFF0000, 0x00FF00);
-//    	registerEntityRenderingHandler(EntityMinion.class, RenderNewMob::new);
+    	RenderingRegistry.registerEntityRenderingHandler(EntityMinion.class, 
+    			new	RenderSilverfish(null));
     }
 }
