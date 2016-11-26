dd66680c4d105345532def5cb854317197b0e151
AIeq <pietari.kokko@aalto.fi>
2016-11-26 14:00:57 +0200

    added dummy genome

---
+++ src/main/resources/python/Genome.py
@@ -0,0 +1,12 @@
+class Genome:  
+  def __init__(self, parents = None): #[Genome, Genome]
+        self.genes = None
+        
+  def getMove(self, input): # color
+    return "forward";
+    
+  def getPhysicalProperties(self):
+    return [color: "black", speed:1, health: 1, damage: 1, lifetime: 20];
+    
+  def __repr__(self):
+    return str(self.genes)
