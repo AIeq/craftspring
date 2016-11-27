637ad5dce73c006f1d687f73cc9e5a2cb5e5b69a
Vladimir Rogojin <vrogojin@vrogojin.net>
2016-11-27 08:05:28 +0200

    implementing reading of the environment from minions

--- src/main/java/EntityMinion.java
+++ src/main/java/EntityMinion.java
@@ -1,8 +1,20 @@
+import net.minecraft.entity.passive.EntitySheep;
 import net.minecraft.entity.passive.EntityWolf;
+import net.minecraft.item.EnumDyeColor;
 import net.minecraft.world.World;
 
-public class EntityMinion extends EntityWolf {
+public class EntityMinion extends EntitySheep {
 
+	public	enum	Color{
+		BLACK,
+		WHITE,
+		RED,
+		GREEN,
+		BLUE
+	}
+	
+	public	Color	color=Color.BLACK;
+	
 	public EntityMinion(World worldIn) {
 		super(worldIn);
 		// TODO Auto-generated constructor stub
@@ -11,5 +23,14 @@ public class EntityMinion extends EntityWolf {
 		this.targetTasks.taskEntries.clear();
 	}
 
-	
+	public	void	setColor(Color	color)
+	{
+		switch(color){
+		case	BLACK:this.setFleeceColor(EnumDyeColor.BLACK);break;
+		case	WHITE:this.setFleeceColor(EnumDyeColor.WHITE);break;
+		case	RED:this.setFleeceColor(EnumDyeColor.RED);break;
+		case	GREEN:this.setFleeceColor(EnumDyeColor.GREEN);break;
+		case	BLUE:this.setFleeceColor(EnumDyeColor.BLUE);break;
+		}
+	}
 }
